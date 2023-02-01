fa95105e3182d981245ab88a57d9f1fb55689a3e
miradonis <miradonis@web.de>
2023-02-01 10:33:38 +0100

    added files

---
+++ src/pages/SmallStuff.jsx
@@ -0,0 +1,25 @@
+// import React from 'react'
+// import Data from '../data';
+
+// function SmallStuff() {
+//     const smallStuffItems = Data.filter(item => item.typ === "BigStuff");
+
+//     return (
+//         <section className='smallStuffSection'>
+
+//             {smallStuffItems.map((item, key) => (
+//                 <div key={key} className='smallStuffDiv'>
+//                     <img className='smallStuffImg' src={item.img} alt={item.name} />
+//                     <div className='smallStuffText'>
+//                         <h2 className='smallStuffHeading'>{item.name}</h2>
+//                         <p className='smallStuffRoom'>{item.room}</p>
+//                         <p className='smallStuffDescription'>{item.description}</p>
+//                     </div>
+//                 </div>
+//             ))}
+//         </section>
+
+//     )
+// }
+
+// export default SmallStuff
