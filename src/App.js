a65b0188786caa7d5b9074473fb1e09c865d9905
miradonis <miradonis@web.de>
2023-02-02 14:35:18 +0100

    get data from firebase works

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import NavComponent from './components/navComponent/NavComponent';
-import Data from './data';
 import CreateCard from './components/createCard/CreateCard';
 import { db } from './Firebase';
 import { collection, getDocs, addDoc, updateDoc, getDoc, doc } from 'firebase/firestore';
@@ -12,10 +11,8 @@ function App() {
 
 	/* 
 	FIREBASE ------------------ Datenbank
-	Funktoniert noch nicht.
-	Daten aus der Data.js sind alle in Firebase 
+	Wir bekommen die Daten und können sie anzeigen
 	*/
-
 
 	const ref = collection(db, 'hausinventar');
 
@@ -27,26 +24,19 @@ function App() {
 			const data = await getDocs(ref);
 			const docsData = [];
 			data.docs.forEach(doc => {
-				docsData.push(doc.data());
+				console.log(doc.data());
+				setData(doc.data().data);
 			});
-			setData(docsData);
 		};
 		getData();
 	}, []);
 
 	const bigStuffItems = data.filter(item => item.typ === "BigStuff");
-	console.log(bigStuffItems);
-
-
-
-
-
+	const smallStuffItems = data.filter(item => item.typ === "SmallStuff");
+	const notSoBigStuffItems = data.filter(item => item.typ === "NotSoBigStuff");
+	// console.log(bigStuffItems);
+	// console.log("Hallo", data.length);
 	/* ENDE!!!!!!!!! FIREBASE ------------------ Datenbank */
-
-
-	// const bigStuffItems = Data.filter(item => item.typ === "BigStuff");
-	const smallStuffItems = Data.filter(item => item.typ === "SmallStuff");
-	const notSoBigStuffItems = Data.filter(item => item.typ === "NotSoBigStuff");
 
 	return (
 		<div className="App">
@@ -54,7 +44,7 @@ function App() {
 				<NavComponent />
 				<Routes>
 					{/*
-					Daten werden aus der Data.js an die Komponenten übergeben.
+					Daten werden aus Firebase geholt und an die Komponenten übergeben.
 					*/}
 					<Route path="/" element={<Home />} />
 					<Route path="/BigStuff" element={<CreateCard data={bigStuffItems} />} />
